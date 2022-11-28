19d7a45c970a4fa0b0febfc3b9fc212af844039f
mmoser-blaze <mmoser@perforce.com>
2022-11-28 03:18:39 +0200

    update script

--- build/download-github-artifacts.sh
+++ build/download-github-artifacts.sh
@@ -8,11 +8,6 @@ function Help {
         echo "Error: $*"
     fi
 
-    if [[ ! -z "${SHORT_HELP_MODE}" ]]; then
-        echo "-s <source filter> -f <from> -t <to> [-v -h] : find replace in multiple files"
-        exit 1
-    fi
-
 cat <<EOF
 $0 -u <github user> -r <github reporsitory> [-v -h]
 
@@ -20,7 +15,7 @@ for a given github user and repository: downloads all artifacts for the latest r
 
     -u <github user>        - github user
     -r <github repository>  - repository of the given user
-    -o <outputdir>          - optional (default: download to current dir
+    -o <outputdir>          - optional (default: download to current dir)
     
     -v                      - verbose output
     -h                      - show this help text
@@ -28,6 +23,23 @@ for a given github user and repository: downloads all artifacts for the latest r
 EOF
 
 exit 1
+}
+
+# True iff all arguments are executable in $PATH , from: https://stackoverflow.com/questions/6569478/detect-if-executable-file-is-on-users-path
+function assert_bins_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
+  if [[ $? != 0 ]]; then
+    echo "Error: $1 is not in the current path"
+    exit 1
+  fi    
+  if [[ $# -gt 1 ]]; then
+    shift  # We've just checked the first one
+    assert_bins_in_path "$@"
+  fi
 }
 
 USER=""
@@ -59,6 +71,8 @@ while getopts "hvu:r:o:" opt; do
    esac
 done	
 
+assert_bins_in_path "jq" "curl"
+
 if [[ $USER == "" ]]; then
     Help "user not given"
 fi
@@ -66,6 +80,14 @@ fi
 if [[ $REPO == "" ]]; then
     Help "repository not given"
 fi
+
+if [[ $DIR != "" ]]; then 
+    if [[ ! -d "$DIR" ]]; then
+        echo "Error: output directory $DIR does not exist"
+        exit 1
+    fi
+fi     
+
 
 LATEST_TAG=$(curl -L -s -S  -H "Accept: application/json" "https://github.com/${USER}/${REPO}/releases/latest" | jq --raw-output .tag_name)
 
