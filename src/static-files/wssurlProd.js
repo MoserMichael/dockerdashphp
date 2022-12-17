801c8f093dd5f0c7254540d7eac7882a01db6374
mmoser-blaze <mmoser@perforce.com>
2022-12-17 14:27:14 +0200

    another try

--- src/static-files/wssurlProd.js
+++ src/static-files/wssurlProd.js
@@ -3,4 +3,5 @@
 function makeWssUrl(uri) {
     let wsProtocol = location.protocol === 'http:' ? 'ws' : 'wss';
     let port = parseInt(location.port);
+    return wsProtocol + '://' + location.hostname + ':' + port + '/' + uri;
 }
