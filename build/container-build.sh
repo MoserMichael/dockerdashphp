deeff721b250a3e11262d41f0bd9cac6f2f83aca
mmoser-blaze <mmoser@perforce.com>
2022-12-22 13:18:26 +0200

    remove container builder

--- build/container-build.sh
+++ build/container-build.sh
@@ -11,8 +11,12 @@ pushd ${TMPFILE}
 cp src/static-files/wssurlProd.js src/static-files/wssurl.js
 
 docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 -f Dockerfile -t ghcr.io/mosermichael/phpdocker-mm:latest . 
- 
+docker buildx build --platform=linux/amd64,linux/arm64 --tag ghcr.io/mosermichael/phpdocker-mm:latest --push . 
+docker buildx stop
+docker buildx rm 
+
 popd
 
 rm -rf ${TMPFILE}
+
+echo "*** container build completed ***"
